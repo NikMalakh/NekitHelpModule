940a2a2e7a53a9eb5588d75c5f446e732de847d3
AppGrade Mikit <89784990+NikMalakh@users.noreply.github.com>
2021-10-01 15:43:04 +0300

    Update NekitHelpModule.py

--- NekitHelpModule.py
+++ NekitHelpModule.py
@@ -96,27 +96,27 @@ class HelpMod(loader.Module):
         self.is_bot = await client.is_bot()
         self.db = db
     async def mlcmd(self, message):
-    """Вывести ссылку на модуль"""
-    args = utils.get_args_raw(message)
-    if not args:
-        return await message.edit('<b>Ошибка ❌:</b> Отсутствуют аргументы')
+        """Вывести ссылку на модуль"""
+        args = utils.get_args_raw(message)
+        if not args:
+            return await message.edit('<b>Ошибка ❌:</b> Отсутствуют аргументы')
 
-        await message.edit('Ищем...')
+            await message.edit('Ищем...')
 
-        try:
-            f = ' '.join([x.strings["name"] for x in self.allmodules.modules if args.lower() == x.strings["name"].lower()])
-            r = inspect.getmodule(next(filter(lambda x: args.lower() == x.strings["name"].lower(), self.allmodules.modules)))
-            link = str(r).split('(')[1].split(')')[0]
-            if "http" not in link:
-                text = f"Модуль {f}:"
-            else:
-                text = f"<a href=\"{link}\">📎 Ссылка</a> на модуль {f}: <code>{link}</code>"
+            try:
+                f = ' '.join([x.strings["name"] for x in self.allmodules.modules if args.lower() == x.strings["name"].lower()])
+                r = inspect.getmodule(next(filter(lambda x: args.lower() == x.strings["name"].lower(), self.allmodules.modules)))
+                link = str(r).split('(')[1].split(')')[0]
+                if "http" not in link:
+                    text = f"Модуль {f}:"
+                else:
+                    text = f"<a href=\"{link}\">📎 Ссылка</a> на модуль {f}: <code>{link}</code>"
 
-            out = io.BytesIO(r.__loader__.data)
-            out.name = f + ".py"
-            out.seek(0)
+                out = io.BytesIO(r.__loader__.data)
+                out.name = f + ".py"
+                out.seek(0)
 
-            await message.respond(text, file=out)
-            await message.delete()
-        except:
-            return await message.edit("<b>🤔 Неизвестная ошибка</b>")
+                await message.respond(text, file=out)
+                await message.delete()
+            except:
+                return await message.edit("<b>🤔 Неизвестная ошибка</b>")
